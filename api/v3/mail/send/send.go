fae99f986f9912dbf765b9ca7fef2d18ef0621c7
yKanazawa <yakitori009@gmail.com>
2021-06-17 03:23:37 +0900

    initial POST /v3/mail/send

--- api/v3/mail/send/send.go
+++ api/v3/mail/send/send.go
@@ -1,10 +1,49 @@
 package send
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
+
+type PostRequest struct {
+	Personalizations []struct {
+		To []struct {
+			Email string `json:"email"`
+			Name  string `json:"name"`
+		} `json:"to"`
+		Cc []struct {
+			Email string `json:"email"`
+			Name  string `json:"name"`
+		} `json:"cc"`
+		Bcc []struct {
+			Email string `json:"email"`
+			Name  string `json:"name"`
+		} `json:"bcc"`
+	} `json:"personalizations"`
+	From struct {
+		Email string `json:"email"`
+		Name  string `json:"name"`
+	} `json:"from"`
+	ReplyTo struct {
+		Email string `json:"email"`
+		Name  string `json:"name"`
+	} `json:"reply_to"`
+	Subject string `json:"subject"`
+	Content []struct {
+		Type  string `json:"type"`
+		Value string `json:"value"`
+	} `json:"content"`
+	Attachments []struct {
+		Content     string `json:"content"`
+		Type        string `json:"type"`
+		Filename    string `json:"filename"`
+		Disposition string `json:"disposition"`
+		ContentId   string `json:"content_id"`
+	} `json:"attachments"`
+}
 
 type ErrorResponse struct {
 	Errors []struct {
@@ -22,8 +61,16 @@ func GetSend() echo.HandlerFunc {
 
 func PostSend() echo.HandlerFunc {
 	return func(c echo.Context) (err error) {
-		id := c.Param("id")
-		return c.String(http.StatusAccepted, id)
+
+		var postRequest PostRequest
+
+		if err := json.NewDecoder(c.Request().Body).Decode(&postRequest); err != nil {
+			return c.JSON(http.StatusBadRequest, getErrorResponse("Bad Request", nil, nil))
+		}
+
+		// Debug
+		fmt.Printf("postRequest %#v\n", postRequest)
+		return c.String(http.StatusAccepted, "")
 	}
 }
 
@@ -34,7 +81,7 @@ func getErrorResponse (message string, field interface{}, help interface{}) Erro
 		Field   interface{} `json:"field"`
 		Help    interface{} `json:"help"`
     } {
-        "POST method allowed only",
+        message,
 		nil,
 		nil,
     }
