d72dfce0f618596ccd613e0eca3d2434556fb90d
yKanazawa <yakitori009@gmail.com>
2021-06-18 14:45:42 +0900

    refactor: move to model

--- api/v3/mail/send/send.go
+++ api/v3/mail/send/send.go
@@ -6,44 +6,8 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	model "github.com/yKanazawa/sendgrid-dev/model/v3/mail"
 )
-
-type PostRequest struct {
-	Personalizations []struct {
-		To []struct {
-			Email string `json:"email"`
-			Name  string `json:"name"`
-		} `json:"to"`
-		Cc []struct {
-			Email string `json:"email"`
-			Name  string `json:"name"`
-		} `json:"cc"`
-		Bcc []struct {
-			Email string `json:"email"`
-			Name  string `json:"name"`
-		} `json:"bcc"`
-	} `json:"personalizations"`
-	From struct {
-		Email string `json:"email"`
-		Name  string `json:"name"`
-	} `json:"from"`
-	ReplyTo struct {
-		Email string `json:"email"`
-		Name  string `json:"name"`
-	} `json:"reply_to"`
-	Subject string `json:"subject"`
-	Content []struct {
-		Type  string `json:"type"`
-		Value string `json:"value"`
-	} `json:"content"`
-	Attachments []struct {
-		Content     string `json:"content"`
-		Type        string `json:"type"`
-		Filename    string `json:"filename"`
-		Disposition string `json:"disposition"`
-		ContentId   string `json:"content_id"`
-	} `json:"attachments"`
-}
 
 type ErrorResponse struct {
 	Errors []struct {
@@ -62,7 +26,7 @@ func GetSend() echo.HandlerFunc {
 func PostSend() echo.HandlerFunc {
 	return func(c echo.Context) (err error) {
 
-		var postRequest PostRequest
+		var postRequest model.PostRequest
 
 		if err := json.NewDecoder(c.Request().Body).Decode(&postRequest); err != nil {
 			return c.JSON(http.StatusBadRequest, getErrorResponse("Bad Request", nil, nil))
---
+++ model/v3/mail/send.go
@@ -0,0 +1,38 @@
+package send
+
+type PostRequest struct {
+	Personalizations []struct {
+		To []struct {
+			Email string `json:"email"`
+			Name  string `json:"name"`
+		} `json:"to"`
+		Cc []struct {
+			Email string `json:"email"`
+			Name  string `json:"name"`
+		} `json:"cc"`
+		Bcc []struct {
+			Email string `json:"email"`
+			Name  string `json:"name"`
+		} `json:"bcc"`
+	} `json:"personalizations"`
+	From struct {
+		Email string `json:"email"`
+		Name  string `json:"name"`
+	} `json:"from"`
+	ReplyTo struct {
+		Email string `json:"email"`
+		Name  string `json:"name"`
+	} `json:"reply_to"`
+	Subject string `json:"subject"`
+	Content []struct {
+		Type  string `json:"type"`
+		Value string `json:"value"`
+	} `json:"content"`
+	Attachments []struct {
+		Content     string `json:"content"`
+		Type        string `json:"type"`
+		Filename    string `json:"filename"`
+		Disposition string `json:"disposition"`
+		ContentId   string `json:"content_id"`
+	} `json:"attachments"`
+}
