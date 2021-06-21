361b5622de7180d8b8914c6429e4656a28d930c8
yKanazawa <yakitori009@gmail.com>
2021-06-21 18:46:11 +0900

    validate move to model

--- api/v3/mail/send/send.go
+++ api/v3/mail/send/send.go
@@ -1,12 +1,10 @@
 package send
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 	model "github.com/yKanazawa/sendgrid-dev/model/v3/mail"
-	"gopkg.in/go-playground/validator.v9"
 )
 
 type ErrorResponse struct {
@@ -19,56 +17,22 @@ type ErrorResponse struct {
 
 func GetSend() echo.HandlerFunc {
 	return func(c echo.Context) (err error) {
-		return c.JSON(http.StatusMethodNotAllowed, getErrorResponse("POST method allowed only", nil, nil))
+		return c.JSON(http.StatusMethodNotAllowed, model.GetErrorResponse("POST method allowed only", nil, nil))
 	}
 }
 
 func PostSend() echo.HandlerFunc {
 	return func(c echo.Context) (err error) {
-		var postRequest model.PostRequest
+		var postRequest model.PostRequest;
 		if err := postRequest.SetPostRequest(c.Request().Body); err != nil {
-			return c.JSON(http.StatusBadRequest, getErrorResponse("Bad Request", nil, nil))
+			return c.JSON(http.StatusBadRequest, model.GetErrorResponse("Bad Request", nil, nil))
 		}
 
-		validate := validator.New()
-		if err := validate.Struct(postRequest); err != nil {
-			for _, err := range err.(validator.ValidationErrors) {
-
-				fmt.Println("err.Namespace()", err.Namespace())
-				fmt.Println("err.Field()", err.Field())
-				fmt.Println("err.StructNamespace()", err.StructNamespace())
-				fmt.Println("err.StructField()", err.StructField())
-				fmt.Println("err.Tag()", err.Tag())
-				fmt.Println("err.ActualTag()", err.ActualTag())
-				fmt.Println("err.Kind()", err.Kind())
-				fmt.Println("err.Type()", err.Type())
-				fmt.Println("err.Value()", err.Value())
-				fmt.Println("err.Param()", err.Param())
-				fmt.Println()
-				switch err.ActualTag() {
-				case "required":
-					return c.JSON(http.StatusBadRequest, getErrorResponse("Validate Failed", nil, nil))
-				}
-			}
-			return c.JSON(http.StatusBadRequest, getErrorResponse("Validate Failed", nil, nil))
+		statusCode, errorResponse := postRequest.Validate();
+		if (statusCode != http.StatusAccepted) {
+			return c.JSON(statusCode, errorResponse);
 		}
 
 		return c.String(http.StatusAccepted, "")
 	}
-}
-
-func getErrorResponse (message string, field interface{}, help interface{}) ErrorResponse {
-	errorJSON := ErrorResponse{}
-    e := struct {
-		Message string      `json:"message"`
-		Field   interface{} `json:"field"`
-		Help    interface{} `json:"help"`
-    } {
-        message,
-		nil,
-		nil,
-    }
-	errorJSON.Errors = append(errorJSON.Errors, e)
-
-	return errorJSON
 }
--- model/v3/mail/send.go
+++ model/v3/mail/send.go
@@ -2,7 +2,11 @@ package send
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
 	"io"
+
+	"gopkg.in/go-playground/validator.v9"
 )
 
 type PostRequest struct {
@@ -42,6 +46,57 @@ type PostRequest struct {
 	} `json:"attachments"`
 }
 
+type ErrorResponse struct {
+	Errors []struct {
+		Message string      `json:"message"`
+		Field   interface{} `json:"field"`
+		Help    interface{} `json:"help"`
+	} `json:"errors"`
+}
+
 func (postRequest *PostRequest) SetPostRequest(requestBody io.ReadCloser) error {
 	return json.NewDecoder(requestBody).Decode(&postRequest);
+}
+
+func (postRequest *PostRequest) Validate() (int, ErrorResponse) {
+	validate := validator.New()
+	if err := validate.Struct(postRequest); err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+
+			// Debug
+			fmt.Println("err.Namespace()", err.Namespace())
+			fmt.Println("err.Field()", err.Field())
+			fmt.Println("err.StructNamespace()", err.StructNamespace())
+			fmt.Println("err.StructField()", err.StructField())
+			fmt.Println("err.Tag()", err.Tag())
+			fmt.Println("err.ActualTag()", err.ActualTag())
+			fmt.Println("err.Kind()", err.Kind())
+			fmt.Println("err.Type()", err.Type())
+			fmt.Println("err.Value()", err.Value())
+			fmt.Println("err.Param()", err.Param())
+			fmt.Println()
+
+			switch err.ActualTag() {
+			case "required":
+				return http.StatusBadRequest, GetErrorResponse("The subject is required. You can get around this requirement if you use a template with a subject defined or if every personalization has a subject defined.", "subject", "http://sendgrid.com/docs/API_Reference/Web_API_v3/Mail/errors.html#message.subject")
+			}
+		}
+	}
+	return http.StatusAccepted, GetErrorResponse("", nil, nil)
+}
+
+func GetErrorResponse (message string, field interface{}, help interface{}) ErrorResponse {
+	errorJSON := ErrorResponse{}
+    e := struct {
+		Message string      `json:"message"`
+		Field   interface{} `json:"field"`
+		Help    interface{} `json:"help"`
+    } {
+        message,
+		field,
+		help,
+    }
+	errorJSON.Errors = append(errorJSON.Errors, e)
+
+	return errorJSON
 }
