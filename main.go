30f5a852ce4e4e1ff7170bde4250b614d5a5169c
yKanazawa <yakitori009@gmail.com>
2021-07-18 04:03:11 +0900

    fix: SENDGRID_DEV_API_KEY

--- main.go
+++ main.go
@@ -14,9 +14,9 @@ func main() {
 	fmt.Println("SENDGRID_DEV_API_SERVER", os.Getenv("SENDGRID_DEV_API_SERVER"))
 
 	if os.Getenv("SENDGRID_DEV_API_KEY") == "" {
-		os.Setenv("SENDGRID_DEV_APIK_EY", "SG.xxxxx")
+		os.Setenv("SENDGRID_DEV_API_KEY", "SG.xxxxx")
 	}
-	fmt.Println("SENDGRID_DEV_APIKEY", os.Getenv("SENDGRID_DEV_APIKEY"))
+	fmt.Println("SENDGRID_DEV_API_KEY", os.Getenv("SENDGRID_DEV_API_KEY"))
 
 	if os.Getenv("SENDGRID_DEV_SMTP_SERVER") == "" {
 		os.Setenv("SENDGRID_DEV_SMTP_SERVER", "127.0.0.1:1025")
